480e77f56dea3c429ef21059a677726ff34c58eb
Valentin Popescu <valentinpopescu98@gmail.com>
2024-03-16 15:55:02 +0200

    Created scrap Spring Boot project
    Dependencies:
     * Spring Web
     * Spring Data JPA
     * Spring Security
     * MySQL Driver
     * Lombok

---
+++ src/test/java/com/valentinpopescu98/storemanagement/StoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valentinpopescu98.storemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
