692f80100eb477ea5a338251f7319eeaf9842155
Valentin Popescu <valentinpopescu98@gmail.com>
2024-03-17 15:26:48 +0200

    Updated exception handling

--- src/main/java/com/valentinpopescu98/storemanagement/store/ProductRepository.java
+++ src/main/java/com/valentinpopescu98/storemanagement/store/ProductRepository.java
@@ -3,11 +3,9 @@ package com.valentinpopescu98.storemanagement.store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    Optional<Product> findByName(String name);
+    boolean existsByName(String name);
 
 }
--- src/main/java/com/valentinpopescu98/storemanagement/store/ProductService.java
+++ src/main/java/com/valentinpopescu98/storemanagement/store/ProductService.java
@@ -1,6 +1,9 @@
 package com.valentinpopescu98.storemanagement.store;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,93 +16,99 @@ public class ProductService {
     private final ProductRepository repository;
 
     public Product getOne(Long id) {
-        Optional<Product> foundProduct = repository.findById(id);
-        boolean productNotFound = foundProduct.isEmpty();
+        Optional<Product> fetchedProduct = repository.findById(id);
+        boolean productNotFound = fetchedProduct.isEmpty();
 
-        // if the id from the request returns no result, throw exception
+        // if the id from the request returns no result
         if (productNotFound) {
-            throw new IllegalStateException(
-                    String.format("Product with %d ID not found.", id));
+            throw new EntityNotFoundException(
+                    String.format("Product with ID '%d' not found.", id));
         }
 
-        return foundProduct.get();
+        return fetchedProduct.get();
     }
 
     public List<Product> getAll() {
-        return repository.findAll();
+        List<Product> fetchedProducts = repository.findAll();
+        boolean noProductFound = fetchedProducts.isEmpty();
+
+        // if the database responds with no product
+        if (noProductFound) {
+            throw new EmptyResultDataAccessException("No product has been found", 1);
+        }
+
+        return fetchedProducts;
     }
 
     public Product add(Product product) {
-        Optional<Product> foundProduct = repository.findByName(product.getName());
-        boolean productExists = foundProduct.isPresent();
+        String productName = product.getName();
+        boolean nameAlreadyExisting = repository.existsByName(productName);
 
-        // if a product with the same name exists
-        if (productExists) {
-            throw new IllegalStateException(
-                    String.format("Product with name %s exists.", product.getName()));
+        // if a product with the same name exists in the DB
+        if (nameAlreadyExisting) {
+            throw new DataIntegrityViolationException(
+                    String.format("Product with name '%s' already exists.", productName));
         }
 
-        if (product.getName() == null) {
-            throw new IllegalStateException("Product must have a name");
+        // if the added product has conflicting fields
+        if (productName == null) {
+            throw new IllegalArgumentException("Product must have a name");
         }
         if (product.getDescription() == null) {
-            throw new IllegalStateException("Product must have a description");
+            throw new IllegalArgumentException("Product must have a description");
         }
         if (product.getPrice() == null) {
-            throw new IllegalStateException("Product must have a price value");
+            throw new IllegalArgumentException("Product must have a price value");
         }
         if (product.getPrice() == 0) {
-            throw new IllegalStateException("Product must have a price bigger than 0");
+            throw new IllegalArgumentException("Product must have a price bigger than '0'");
         }
         if (product.getStock() == null) {
-            throw new IllegalStateException("Product must have a stock number");
+            throw new IllegalArgumentException("Product must have a stock number");
         }
 
         repository.save(product);
-
         return product;
     }
 
     public Product update(Long id, Product newProduct) {
         Optional<Product> oldProductOptional = repository.findById(id);
-        Optional<Product> newProductOptional = repository.findByName(newProduct.getName());
+        String productName = newProduct.getName();
+        boolean productNotFound = oldProductOptional.isEmpty();
+        boolean nameAlreadyExisting = repository.existsByName(productName);
 
-        boolean oldProductNotFound = oldProductOptional.isEmpty();
-        boolean newProductExists = newProductOptional.isPresent();
-
-        if (oldProductNotFound) {
-            throw new IllegalStateException(
-                    String.format("Product with ID %d not found.", id));
+        // if the id from the request returns no result
+        if (productNotFound) {
+            throw new EntityNotFoundException(
+                    String.format("Product with ID '%d' not found.", id));
         }
 
-        if (newProductExists) {
-            throw new IllegalStateException(
-                    String.format("Product with name %s already exists.", newProduct.getName()));
+        // if a product with the same name exists in the DB
+        if (nameAlreadyExisting) {
+            throw new DataIntegrityViolationException(
+                    String.format("Product with name '%s' already exists.", productName));
         }
 
+        // if the updated product price is '0'
         if (newProduct.getPrice() != null && newProduct.getPrice() == 0) {
-            throw new IllegalStateException("Product must have a price bigger than 0");
+            throw new IllegalArgumentException("Product must have a price bigger than '0'");
         }
 
         Product oldProduct = updateOldDto(oldProductOptional.get(), newProduct);
-
-        repository.save(oldProduct);
-        return oldProduct;
+        return repository.save(oldProduct);
     }
 
-    public String remove(Long id) {
+    public void remove(Long id) {
         Optional<Product> foundProduct = repository.findById(id);
         boolean productNotFound = foundProduct.isEmpty();
 
         if (productNotFound) {
-            throw new IllegalStateException(
-                    String.format("Product with %d ID not found.", id));
+            throw new EntityNotFoundException(
+                    String.format("Product with ID '%d' not found.", id));
         }
 
         Product product = foundProduct.get();
         repository.delete(product);
-
-        return product + " deleted";
     }
 
     private static Product updateOldDto(Product oldProduct, Product newProduct) {
