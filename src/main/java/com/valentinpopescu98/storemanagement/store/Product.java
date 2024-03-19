d77f27decb70b3df04d4f98ae9ca2023847930d1
Valentin Popescu <valentinpopescu98@gmail.com>
2024-03-19 23:13:30 +0200

    Update product DTO with validations

--- src/main/java/com/valentinpopescu98/storemanagement/store/Product.java
+++ src/main/java/com/valentinpopescu98/storemanagement/store/Product.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -20,12 +22,18 @@ public class Product {
     @SequenceGenerator(allocationSize = 1, name = "product_sequence", sequenceName = "product_sequence")
     @GeneratedValue(generator = "product_sequence", strategy = IDENTITY)
     private final Long id;
+    @NotNull(message = "Product must have a name")
+    @NotEmpty(message = "Product name must not be blank")
     private String name;
+    @NotNull(message = "Product must have a description")
+    @NotEmpty(message = "Product description must not be blank")
     private String description;
     // price in cents
+    @NotNull(message = "Product must have a price")
     private Long price;
     // manufacturer's suggested retail price in cents
     private Long msrp;
+    @NotNull(message = "Product must have a stock")
     private Long stock;
 
     // no args constructor to enable JPA for hydrating entities
