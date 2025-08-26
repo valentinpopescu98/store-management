015f468b417db12fa65ae9eae3216d8bd887542f
Valentin Popescu <contact@valentinpopescu.com>
2025-08-26 17:23:47 +0300

    First commit

---
+++ src/test/java/com/valentinpopescu/store/StoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valentinpopescu.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
