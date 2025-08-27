54678e80e486a4f17036e8b111e785df1ae83c2c
Valentin Popescu <contact@valentinpopescu.com>
2025-08-27 11:53:08 +0300

    Added bean validation (pre-DB) for model

--- src/main/java/com/valentinpopescu/store/product/model/Product.java
+++ src/main/java/com/valentinpopescu/store/product/model/Product.java
@@ -1,6 +1,9 @@
 package com.valentinpopescu.store.product.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,10 +20,17 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
     @Column(nullable = false, unique = true)
     private String productCode;
+
+    @NotBlank
     @Column(nullable = false)
     private String name;
+
+    @NotNull
+    @Positive
     @Column(nullable = false, precision = 19, scale = 2)
     private BigDecimal price;
 
