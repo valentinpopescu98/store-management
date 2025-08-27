d4b3fc3bbbdbb1dcf5769e24b846a47299d76499
Valentin Popescu <contact@valentinpopescu.com>
2025-08-27 11:43:17 +0300

    Added custom not found exception (404)

---
+++ src/main/java/com/valentinpopescu/store/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.valentinpopescu.store.exceptions;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
