44919224483353b806eb9f712ee846f0920ece87
Valentin Popescu <contact@valentinpopescu.com>
2025-09-03 12:48:54 +0300

    Document controller API

--- src/main/java/com/valentinpopescu/store/product/controller/ProductController.java
+++ src/main/java/com/valentinpopescu/store/product/controller/ProductController.java
@@ -4,6 +4,9 @@ import com.valentinpopescu.store.product.dto.PriceChangeRequest;
 import com.valentinpopescu.store.product.dto.ProductCreateRequest;
 import com.valentinpopescu.store.product.dto.ProductResponse;
 import com.valentinpopescu.store.product.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@SecurityRequirement(name = "basicAuth")
 @RestController
 @Validated
 @RequestMapping("/api/products")
@@ -22,6 +26,8 @@ public class ProductController {
 
     private final ProductService service;
 
+    @Operation(summary = "Add product", description = "Add a product and return it")
+    @ApiResponse(responseCode = "201", description = "Product created")
     @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -29,18 +35,24 @@ public class ProductController {
         return service.add(request);
     }
 
+    @Operation(summary = "Find product by product code", description = "Find a product by product code and return it")
+    @ApiResponse(responseCode = "200", description = "Product found")
     @PreAuthorize("hasAnyRole('USER','ADMIN')")
     @GetMapping("/{productCode}")
     public ProductResponse find(@PathVariable @NotBlank String productCode) {
         return service.findByProductCode(productCode);
     }
 
+    @Operation(summary = "Find products", description = "List all products")
+    @ApiResponse(responseCode = "200", description = "Products fetch successful")
     @PreAuthorize("hasAnyRole('USER','ADMIN')")
     @GetMapping
     public List<ProductResponse> findAll() {
         return service.findAll();
     }
 
+    @Operation(summary = "Change price", description = "Find product by product code, change its price and return it")
+    @ApiResponse(responseCode = "200", description = "Product's price changed")
     @PreAuthorize("hasAnyRole('ADMIN')")
     @PatchMapping("/{productCode}/price")
     public ProductResponse changePrice(
@@ -49,6 +61,8 @@ public class ProductController {
         return service.changePrice(productCode, request);
     }
 
+    @Operation(summary = "Delete product", description = "Find product by product code and delete it")
+    @ApiResponse(responseCode = "204", description = "Product deleted")
     @PreAuthorize("hasAnyRole('ADMIN')")
     @DeleteMapping("/{productCode}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
