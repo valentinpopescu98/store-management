41d5b2bdc7c23b69572d57b00e7011060d7cb47d
Valentin Popescu <contact@valentinpopescu.com>
2025-09-03 12:48:41 +0300

    Create configuration for basic authentication

---
+++ src/main/java/com/valentinpopescu/store/config/OpenApiConfig.java
@@ -0,0 +1,14 @@
+package com.valentinpopescu.store.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "basicAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "basic"
+)
+public class OpenApiConfig {
+}
